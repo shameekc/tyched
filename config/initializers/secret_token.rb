a85bf2c004d1979f3c74853333012f33b97dad0b
Shameek Chakravarty <shameekc@macbook.local>
2010-12-28 19:29:03 +0530

    tyche

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tyche::Application.config.secret_token = 'ae225093e1be7a3ebb1ef8e3fe62623e0b0b8435ed169c723593a496d3ee06a795ad388a67549c73b918e399b847a10efbd29fddc50a6d095706e183064db478'
