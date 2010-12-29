3a6a879cdb655b03c813ef6430e9a95722e6d449
Shameek Chakravarty <shameekc@macbook.local>
2010-12-29 13:46:50 +0530

    setting actual paths for about contact etc

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Tyche::Application.routes.draw do
-  get "pages/home"
-
-  get "pages/contact"
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
   
-  get "pages/about"
-
+  root :to => 'pages#home'
+  
   resources :wishes
 
   resources :users
